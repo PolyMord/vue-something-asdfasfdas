b9d978b25ef37b9f387cfcee7de56124ef796aa6
PolyMord <stanislavmalickiy@gmail.com>
2020-06-17 00:11:21 +0300

    change typer from text to texts

--- src/views/Typer.vue
+++ src/views/Typer.vue
@@ -4,24 +4,27 @@
     <div class="content">
       <div class="stats">
         <span>Score: {{ score }}</span>
-        <span>Time: {{ (this.time * 0.01).toFixed(2) }}</span>
+        <span>Time: </span>
       </div>
       <p class="text">
-        {{ text }}
+        <span style="background: green; color: white;">{{ typedWords.join(' ') }}</span>
+        <span style="background: yellow; color: black;">{{ currentWord }}</span>
+        <span>{{ untypedWords.join(' ')}}</span>
       </p>
       <input
         type="text"
         class="input"
         v-model="input"
         @keyup.stop="generalKeys"
+        @input.stop="matchWord"
       />
       <div class="end">
-        <span
-          ><strong>WPM: {{ WPM }}</strong></span
-        >
-        <span
-          ><strong>CPM: {{ CPM }}</strong></span
-        >
+        <span>
+          <strong>WPM: </strong>
+        </span>
+        <span>
+          <strong>CPM: </strong>
+        </span>
       </div>
     </div>
   </div>
@@ -32,59 +35,59 @@ export default {
   name: "Typer",
 
   data: () => ({
-    text: "",
+    text: "lorem ipsum dolor sit amet eat play game work elit",
     input: "",
-    value: [],
-    texts: [
-      "lorem",
-      "ipsum",
-      "dolor",
-      "sit",
-      "amet",
-      "eat",
-      "play",
-      "game",
-      "work",
-      "elit",
-    ],
+    allWords: [],
+    untypedWords: [],
+    typedWords: [],
+    currentWord: "",
+    idx: 0,
+    charIdx: 0,
     score: 0,
-    time: 0,
-    WPM: 0,
-    CPM: 0,
   }),
 
   methods: {
-    generalKeys(e) {
-      if (e.key === " ") {
-        if (this.input.trim() === this.text) {
-          this.input = ""
-          this.score++
-          this.randText()
-        } else console.log("ыыы")
+    takeWord(idx) {
+      this.untypedWords.shift()
+      this.currentWord = this.allWords[idx]
+    },
+
+    matchWord(e) {
+      if (this.currentWord) {
+        const word = this.currentWord.split("")
+        if (e.target.value[this.charIdx] === word[this.charIdx]) {
+          console.log(word[this.charIdx])
+          this.charIdx++
+        }
       }
     },
 
-    timer() {
-      const time = setInterval(() => {
-        this.time++
-        if (this.time === 6000) {
-          clearInterval(time)
-          this.WPM = this.score
-          this.CPM = this.score * 5
+    generalKeys(e) {
+      if (e.key === " ") {
+        if (this.input.trim() === this.currentWord) {
+          this.input = ""
+          this.score++
+          this.charIdx = 0
+          this.typedWords.push(this.allWords[this.idx])
+          this.idx++
+          this.takeWord(this.idx)
+          console.log("норм")
+        } else {
+          console.log("ошибка")
         }
-      }, 10)
+        if (this.allWords.length === this.idx) console.log("конец")
+      }
     },
 
-    randText() {
-      return (this.text = this.texts[
-        Math.floor(Math.random() * this.texts.length)
-      ])
+    init() {
+      this.allWords = this.text.split(" ")
+      this.untypedWords = [...this.allWords]
+      this.takeWord(this.idx)
     },
   },
 
   mounted() {
-    this.timer()
-    this.randText()
+    this.init()
   },
 }
 </script>
@@ -96,29 +99,35 @@ export default {
   align-items: center;
 }
 .content {
+  width: 300px;
+  height: 250px;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
   border: thin solid black;
   border-radius: 10px;
 }
 .text {
-  border: thin solid black;
-  border-radius: 10px;
-  height: 100px;
-  width: 200px;
+  white-space: wrap;
+  padding: 10px;
+  flex-grow: 1;
   display: flex;
   justify-content: center;
   align-items: center;
-}
-.input {
   border: thin solid black;
   border-radius: 10px;
+}
+.input {
   box-sizing: border-box;
+  padding-left: 5px;
   height: 30px;
-  width: 200px;
   outline: none;
+  border: thin solid black;
+  border-radius: 10px;
 }
 .stats,
 .end {
+  margin-top: 10px;
   display: flex;
   justify-content: space-between;
 }
