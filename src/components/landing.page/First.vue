8587e52989e72de0afb027ea1a8856516e351fce
PolyMord <stanislavmalickiy@gmail.com>
2020-06-19 21:02:20 +0300

    init Landing, install AOS

--- src/components/landing.page/First.vue
+++ src/components/landing.page/First.vue
@@ -1,7 +1,96 @@
 <template>
-  <div>This is a First</div>
+  <div class="first">
+    <div class="head">
+      <h1 class="head__wrap">
+        <div data-aos="fade" class="head__item">This is a landing page</div>
+      </h1>
+    </div>
+    <div class="text">
+      <p data-aos="fade-left">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        asperiores esse ullam at vero sit illo. Modi, dolores. Atque
+        necessitatibus voluptas illo architecto adipisci similique perferendis
+        magnam, natus nulla officiis? Nemo ullam porro quibusdam ut dicta
+        accusantium enim alias animi sapiente iusto veniam quis eum molestias
+        hic, voluptas tempora architecto aperiam quam dolorem dolore cupiditate
+        molestiae. Autem expedita quisquam totam. Omnis dolores, ab nam repellat
+        id architecto ut fugiat itaque vel, at quod ullam. Magni illo nisi amet,
+        cupiditate, officia autem fuga est recusandae voluptate odio nesciunt ad
+        error quis! Ut ducimus aspernatur quos. Quia nostrum unde temporibus
+        quis iusto asperiores. Iusto voluptate quaerat labore vero tempora
+        numquam, rerum laboriosam deserunt necessitatibus ipsam accusantium
+        reprehenderit ratione et explicabo suscipit praesentium! Explicabo
+        possimus soluta officiis reprehenderit veritatis nostrum inventore
+        obcaecati. Necessitatibus laborum iste, voluptatum accusamus maxime
+        culpa vel eum doloribus placeat tempora enim fugit non animi quia odit
+        iure eaque ipsa?
+      </p>
+    </div>
+    <div class="img">
+      <img data-aos="fade-right" :src="imgFive" alt="" />
+    </div>
+    <div class="type-text">
+      <span>Lorem, ipsum.</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import imgFive from "@/assets/landing.assets/img/imgFive.png"
+
+export default {
+  data: () => ({
+    imgFive,
+  }),
+}
 </script>
+
+<style scoped>
+@import "../../assets/landing.assets/landing.css";
+
+.first {
+  height: 100vh;
+}
+
+.text {
+  border: thin solid black;
+}
+.text p {
+  width: 50%;
+  text-align: start;
+  text-indent: 1rem;
+}
+
+.img {
+  border: thin solid black;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.type-text {
+  border: thin solid black;
+  height: 16vh;
+}
+
+.head {
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.head__wrap {
+  margin: 0;
+  position: relative;
+  height: 3rem;
+  width: 35vw;
+}
+.head__item {
+  position: absolute;
+  white-space: nowrap;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  animation: noise-shift 4s linear infinite alternate-reverse;
+}
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,7 +1,77 @@
 <template>
   <div class="landing">
-    <h1>This is a landing page</h1>
     <First />
+    <div class="container">
+      <p data-aos="fade-up">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        asperiores esse ullam at vero sit illo. Modi, dolores. Atque
+        necessitatibus voluptas illo architecto adipisci similique perferendis
+        magnam, natus nulla officiis? Nemo ullam porro quibusdam ut dicta
+        accusantium enim alias animi sapiente iusto veniam quis eum molestias
+        hic, voluptas tempora architecto aperiam quam dolorem dolore cupiditate
+        molestiae. Autem expedita quisquam totam. Omnis dolores, ab nam repellat
+        id architecto ut fugiat itaque vel, at quod ullam. Magni illo nisi amet,
+        cupiditate, officia autem fuga est recusandae voluptate odio nesciunt ad
+        error quis! Ut ducimus aspernatur quos. Quia nostrum unde temporibus
+        quis iusto asperiores. Iusto voluptate quaerat labore vero tempora
+        numquam, rerum laboriosam deserunt necessitatibus ipsam accusantium
+        reprehenderit ratione et explicabo suscipit praesentium! Explicabo
+        possimus soluta officiis reprehenderit veritatis nostrum inventore
+        obcaecati. Necessitatibus laborum iste, voluptatum accusamus maxime
+        culpa vel eum doloribus placeat tempora enim fugit non animi quia odit
+        iure eaque ipsa?
+      </p>
+      <p data-aos="fade-up">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        asperiores esse ullam at vero sit illo. Modi, dolores. Atque
+        necessitatibus voluptas illo architecto adipisci similique perferendis
+        magnam, natus nulla officiis? Nemo ullam porro quibusdam ut dicta
+        accusantium enim alias animi sapiente iusto veniam quis eum molestias
+        hic, voluptas tempora architecto aperiam quam dolorem dolore cupiditate
+        molestiae. Autem expedita quisquam totam. Omnis dolores, ab nam repellat
+        id architecto ut fugiat itaque vel, at quod ullam. Magni illo nisi amet,
+        cupiditate, officia autem fuga est recusandae voluptate odio nesciunt ad
+        error quis! Ut ducimus aspernatur quos. Quia nostrum unde temporibus
+        quis iusto asperiores. Iusto voluptate quaerat labore vero tempora
+        numquam, rerum laboriosam deserunt necessitatibus ipsam accusantium
+        reprehenderit ratione et explicabo suscipit praesentium! Explicabo
+        possimus soluta officiis reprehenderit veritatis nostrum inventore
+        obcaecati. Necessitatibus laborum iste, voluptatum accusamus maxime
+        culpa vel eum doloribus placeat tempora enim fugit non animi quia odit
+        iure eaque ipsa?
+      </p>
+      <p data-aos="fade-up">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        asperiores esse ullam at vero sit illo. Modi, dolores. Atque
+        necessitatibus voluptas illo architecto adipisci similique perferendis
+        magnam, natus nulla officiis? Nemo ullam porro quibusdam ut dicta
+        accusantium enim alias animi sapiente iusto veniam quis eum molestias
+        hic, voluptas tempora architecto aperiam quam dolorem dolore cupiditate
+        molestiae. Autem expedita quisquam totam. Omnis dolores, ab nam repellat
+        id architecto ut fugiat itaque vel, at quod ullam. Magni illo nisi amet,
+        cupiditate, officia autem fuga est recusandae voluptate odio nesciunt ad
+        error quis! Ut ducimus aspernatur quos. Quia nostrum unde temporibus
+        quis iusto asperiores. Iusto voluptate quaerat labore vero tempora
+        numquam, rerum laboriosam deserunt necessitatibus ipsam accusantium
+        reprehenderit ratione et explicabo suscipit praesentium! Explicabo
+        possimus soluta officiis reprehenderit veritatis nostrum inventore
+        obcaecati. Necessitatibus laborum iste, voluptatum accusamus maxime
+        culpa vel eum doloribus placeat tempora enim fugit non animi quia odit
+        iure eaque ipsa?
+      </p>
+
+      <div class="lorem">
+        <span v-for="s of typedText(this.text, 50)" :key="s.delay">
+          <span
+            data-aos-offset="50"
+            data-aos="fade"
+            :data-aos-delay="s.delay"
+            data-aos-duration="50"
+            >{{ s.value }}</span
+          >
+        </span>
+      </div>
+    </div>
     <Second />
     <Third />
   </div>
@@ -19,5 +89,43 @@ export default {
     Second,
     Third,
   },
+
+  data: () => ({
+    text: "Lorem ipsum",
+  }),
+
+  methods: {
+    typedText(text, step = 100) {
+      /**
+       * @param {number} step - 50, 100, 150 etc.
+       */
+
+      if (step < 50) step = 50
+      if (step * text.length > 3000) step = 250
+      if (step % 50 !== 0) step -= step % 50
+
+      let delay = 0
+      return text.split("").map(l => {
+        return {
+          value: l,
+          delay: (delay += step),
+        }
+      })
+    },
+  },
 }
 </script>
+
+<style scoped>
+.container {
+  margin: 0 35vw;
+}
+.container p {
+  text-align: start;
+  text-indent: 1rem;
+}
+.lorem {
+  display: inline-block;
+  font-size: 2rem;
+}
+</style>
